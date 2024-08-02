97dc85fce04dd2b4e8b17f98e23c048e42e3a608
Jinseonghyun <jinsunghyun9@gmail.com>
2024-08-02 17:59:54 +0900

    사용자와 주문 엔티티 설계 및 의존관계 형성중

---
+++ src/main/java/com/jin/online_food_ordering/model/User.java
@@ -0,0 +1,32 @@
+package com.jin.online_food_ordering.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private USER_ROLE role;
+
+    private List<Order> orders = new ArrayList<>();
+
+}
